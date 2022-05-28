d1ad938b9f35452bcd5595bef774ffd64f357039
facu <urionafacu@gmail.com>
2022-05-28 14:25:08 -0300

    feat: added schedule message

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import bot, { sendMessage } from './bot.ts';
-import { daily, everyMinute } from 'cron';
+import { cron, daily, everyMinute } from 'cron';
 import { checkDaiPrice, ping } from 'jobs';
 
 sendMessage('Bot is online');
 
 // Every days check if the bot is alive
 daily(ping);
+
+// Send me message at 9:00 AM every day
+cron('* * 12', () => {
+	sendMessage('Bot is alive');
+});
 
 everyMinute(async () => {
 	try {
