75318dd347844f345f2efc8d344260318ef1ced5
facu <urionafacu@gmail.com>
2022-05-28 14:14:22 -0300

    feat: added init message

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import bot, { sendMessage } from './bot.ts';
 import { daily, everyMinute } from 'cron';
 import { checkDaiPrice, ping } from 'jobs';
 
+sendMessage('Bot is online');
+
 // Every days check if the bot is alive
 daily(ping);
 
